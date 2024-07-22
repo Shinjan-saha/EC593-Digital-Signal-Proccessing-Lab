78f152f96760354e0e9625b0cb746b9ef9563226
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-22 20:53:24 +0530

    Ramp Done

---
+++ EXP1/Generating Ramp Signal/Continious-Domain.m
@@ -0,0 +1,7 @@
+m=1;
+t=0:0.5:50;
+y=m*t;
+plot(t,y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Ramp Continuous Domain');
---
+++ EXP1/Generating Ramp Signal/Discreet-Domain.m
@@ -0,0 +1,7 @@
+m=1;
+t=0:0.5:50;
+y=m*t;
+stem(t,y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Ramp Discrete Domain');
