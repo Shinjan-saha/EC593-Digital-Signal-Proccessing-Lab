b0d629c89ae647c0207352d927bbdd46aa717611
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-22 21:06:21 +0530

    Impulse signal Done

---
+++ EXP1/Generating-Impulse-Signal/Continious-Domain.m
@@ -0,0 +1,5 @@
+y=[zeros(1,50),ones(1,1)];
+plot(y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Impulse Signal Continuous Domain');
---
+++ EXP1/Generating-Impulse-Signal/Discreet-Domain.m
@@ -0,0 +1,5 @@
+y=[zeros(1,50),ones(1,1)];
+stem(y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Impulse Signal Discrete Domain');
