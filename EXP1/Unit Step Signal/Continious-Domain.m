ce3d4586d863e3ea5490244bf352b65774e8475f
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-22 21:00:28 +0530

    Added Unit Step

---
+++ EXP1/Unit Step Signal/Continious-Domain.m
@@ -0,0 +1,5 @@
+y=[zeros(1,50),ones(1,50)];
+plot(y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Unit Step Signal Continuous Domain');
---
+++ EXP1/Unit Step Signal/Discreet-Domain.m
@@ -0,0 +1,5 @@
+y=[zeros(1,50),ones(1,50)];
+stem(y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Unit Step Signal Discrete Domain');
