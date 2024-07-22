81ae90e043880c9d16f34f3e2272682f418bb255
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-22 21:12:40 +0530

    Added tri

---
+++ EXP1/Generating Triangular Signal/Continious-Domain.m
@@ -0,0 +1,5 @@
+y=[zeros(1,1),ones(1,1),zeros(1,1)];
+plot(y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Triangular Signal Continuous Domain');
---
+++ EXP1/Generating Triangular Signal/Discreet-Domain.m
@@ -0,0 +1,5 @@
+y=[zeros(1,1),ones(1,1),zeros(1,1)];
+stem(y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Triangular Signal Discrete Domain');
