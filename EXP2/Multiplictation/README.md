e8d34c31a1ed865a0a27af0ee847d8fa8084c0cc
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-29 18:39:58 +0530

    Added proper multiplictation Code

--- EXP2/Multiplictation/README.md
+++ EXP2/Multiplictation/README.md
@@ -4,12 +4,12 @@
 ## Command Window
 
 ```bash
-x1=[2 5 6 3];
-n1= [0 1 2 3];
-x2= [5 9 -8 6];
-n2= [1 2 3 4];
-add(x1,n1,x2,n2)
+x1 = [2 5 6 3];
+n1 = [0 1 2 3];
+x2 = [5 9 -8 6];
+n2 = [1 2 3 4];
+multiplication(x1, n1, x2, n2);
 ```
 
 ## Output 
-<img src='../img/multi1.png'>
+<img src='../img/multi.png'>
--- EXP2/Multiplictation/multiplication.m
+++ EXP2/Multiplictation/multiplication.m
@@ -1,13 +1,24 @@
-function add(x1, n1, x2,n2)
-    m1=(min(min(n1),min(n2)));
-    m2=(max(max(n1),max(n2)));
-    t= (m1:m2);
-    y1= [zeros(1,length(t))];
-    y2= [zeros(1,length(t))];
-    y1 (find((t>=min(n1)&t<=max(n1))==1))=x1;
-    y2(find((t>=min(n2)&t<=max(n2))==1))=x2;
-    y3=y1+y2;
-    subplot(1,3,1),stem(t,y1,'*');
-    subplot(1,3,2),stem(t,y2,'*');
-    subplot(1,3,3),stem(t,y3,'*');
+function multiplication(x1, n1, x2, n2)
+  
+    m1 = min(min(n1), min(n2));
+    m2 = max(max(n1), max(n2));
+    
+    
+    t = m1:m2;
+    
+
+    y1 = zeros(1, length(t));
+    y2 = zeros(1, length(t));
+    
+    
+    y1(ismember(t, n1)) = x1;
+    y2(ismember(t, n2)) = x2;
+    
+    
+    y3 = y1 .* y2;
+    
+   
+    subplot(1, 3, 1), stem(t, y1, '*');
+    subplot(1, 3, 2), stem(t, y2, '*');
+    subplot(1, 3, 3), stem(t, y3, '*');
 end
